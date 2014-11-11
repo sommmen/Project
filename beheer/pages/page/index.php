a943df7a514f970d3d4422e57e7f091f80301371
sommmen <Dion_nl@yahoo.co.uk>
2014-11-11 12:45:05 +0100

    drop-down menu

--- beheer/pages/page/index.php
+++ beheer/pages/page/index.php
@@ -1,10 +1,26 @@
-<?php
 
+
+<table><th>Titel</th><th>link</th><th>aangemaakt</th><th>laatst bewerkt</th><th>zichtbaar</th>
+<?php
 $query = "SELECT * FROM page";
 $result = $mysqli->query($query);
-
-print('<table><th>Titel</th><th>link</th><th>aangemaakt</th><th>laatst bewerkt</th><th>zichtbaar</th>');
-while ($page = $result->fetch_object()) {
-    print("<tr><td>$page->title</td><td>$page->slug</td><td>$page->created</td><td>$page->last_modified</td><td>$page->published</td></tr>");
+    while ($page = $result->fetch_object()) {
+        if($page->published == 1){
+            $published = "ja";
+        } else {
+            $published = "nee";
+        }
+    print("<tr><td>$page->title</td><td>$page->slug</td><td>$page->created</td><td>$page->last_modified</td><td>$published</td></tr>");
 }
-print('</table>');
+?>
+</table>
+<select name="carlist" form="form">
+<?php
+    while ($page = $result->fetch_object()) {
+        print("<option value=\"$page->title\">$page->title</option>");
+    }
+?>
+</select>
+<form action="/page.php" method="post" id="form">
+    <input type="submit" name="submit">
+</form>
