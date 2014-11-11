942f8100e50fb88a4e0bd5220dd1db3ef2931613
sommmen <Dion_nl@yahoo.co.uk>
2014-11-11 21:21:43 +0100

    delete.php gepopt. nog even id uitzoeken op school morn'

--- beheer/pages/page/delete.php
+++ beheer/pages/page/delete.php
@@ -1,9 +1,15 @@
 <?php
 
-/* 
- * Dion leurink s1080954
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$id = get(); //nuja hoe je dan die /page/id pakt
+$query = "DELETE FROM pages WHERE id = $id";
 
-redirect("page.php");
+if (!$mysqli->query($query)) {
+?>
+<meta http-equiv="refresh" content="5; url=/page.php" />
+<h1>Error, deze pagina bestaat niet of is reeds verwijderd.</h1>
+<p> u wordt teruggestuurt. </p>
+<?php
+} else {
+    redirect("page.php");
+}
+
