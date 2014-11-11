5c2ceb2ba67e0ac5f8b43e46ac9e177d080bb89f
Dubehh <eelco1997@gmail.com>
2014-11-11 14:16:01 +0100

    Projects Overview

--- beheer/pages/projects/index.php
+++ beheer/pages/projects/index.php
@@ -3,7 +3,7 @@
 $query = "SELECT * FROM projects";
 $result = $mysqli->query($query);
     while ($page = $result->fetch_object()) {
-         print("<tr><td>$page->title</td><td>$page->uid</td><td>$page->created</td><td><a href=\"/beheer/projects/edit/$page->id\">Edit</a>\<a href=\"/beheer/projects/delete/$page->id\">Delete</a></td></tr>");
+         print("<tr><td>$page->title</td><td>$page->uid</td><td>$page->created</td><td><a href=\"/beheer/projects/edit/$page->title\">Edit</a>\<a href=\"/beheer/projects/delete/$page->title\">Delete</a></td></tr>");
     }
 ?>
 </table>
