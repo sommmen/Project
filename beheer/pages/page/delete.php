90fcd5c49bb24bedc660af852d05639fc910fdb8
sommmen <Dion_nl@yahoo.co.uk>
2014-11-11 20:39:09 +0100

    alles in apparte php bestanden

---
+++ beheer/pages/page/delete.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * Dion leurink s1080954
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+redirect("page.php");
--- beheer/pages/page/index.php
+++ beheer/pages/page/index.php
@@ -1,21 +1,3 @@
-<script type="text/javascript">
-    function getSlug(Text)
-    {
-        return Text
-                .toLowerCase()
-                .replace(/ /g, '-')
-                .replace(/[^\w-]+/g, '')
-                ;
-    }
-
-    function updateValue() {
-        var title = document.getElementById("title").value;
-        document.getElementById("slug").value = getSlug(title);
-    }
-</script>
-
-
-
 <table>
     <tr>
         <th>Titel</th>
@@ -50,41 +32,3 @@
     ?>
 </table>
 <br>
-<h1>Nieuwe pagina</h1>
-<form action="" method="post">
-    <label>Titel</label>
-    <input type="text" name="titel" onkeyup="updateValue()" id="title">
-    <label>Onderschrift</label>
-    <input type="text" name="description">
-    <label>Doelmap</label>
-    <input type="text" name="slug" value="" id="slug">
-    <label>Tekst</label>
-    <textarea rows="4" cols="50" name="body"></textarea>
-
-    <input type="submit" name="submit" value="versturen">
-</form>
-
-<?php
-//TODO als je nu F5 drukt nadat je een pagina hebt toegevoegd, voegt hij de zelfde pagina weer toe.
-//TODO checken of de page slug al bestaat of niet.
-//TODO liefst gwn een normale pagina toevoegen pagina, zodat je alles in 1 keer kan invullen. HEURISTIEKEN VAN NIELSONNAZI?
-
-if (isset($_POST["submit"])) {
-    if (!empty($_POST["titel"])) {
-        $titel = post('titel');
-        $description = post('description');
-        $body = post('body');
-        $slug = urlencode(strtolower($slug));
-
-        $query = "SELECT * FROM page WHERE slug = \"$slug\"";
-        $result = $mysqli->query();
-        if (!$mysqli->query($query)) {
-            $query = "INSERT INTO page (title, slug, published, in_nav, description, body) VALUES (\"$titel\",\"$slug\", 0, 0, \"$description\", \"$body\")";
-            if (!$mysqli->query($query)) {
-                echo $mysqli->error;
-            }
-        } else {
-            echo "vul een titel in.";
-        }
-    }
-}
