1051347f3378dc8cb9ff1c38f5e58d13c7a049fa
kevin889 <kevin@pijning.nl>
2014-11-11 17:21:28 +0100

    Comments (TODO) toegevoegd bij scripts van dion.

--- beheer/pages/page/index.php
+++ beheer/pages/page/index.php
@@ -10,6 +10,8 @@ $result = $mysqli->query($query);
         } else {
             $published = "nee";
         }
+        //TODO dit even netjes weergeven, zoals het in een normaal html bestand ook moet.
+        //Je kan de html gwn zonder php maken, en dan elke keer <?php echo $page->var; doen, is overzichtelijker voor iedereen.
     print("<tr><td>$page->title</td><td>$page->slug</td><td>$page->created</td><td>$page->last_modified</td><td>$published</td><td><a href=\"/beheer/page/edit/$page->id\">Edit</a>\<a href=\"/beheer/page/delete/$page->id\" onClick = \"return confirm('weet je het zeker? verwijderen is definitief')\">Delete</a></td></tr>");
 }
 ?>
@@ -23,6 +25,10 @@ $result = $mysqli->query($query);
 </form>
 
 <?php
+//TODO als je nu F5 drukt nadat je een pagina hebt toegevoegd, voegt hij de zelfde pagina weer toe.
+//TODO checken of de page slug al bestaat of niet.
+//TODO liefst gwn een normale pagina toevoegen pagina, zodat je alles in 1 keer kan invullen. HEURISTIEKEN VAN NIELSONNAZI?
+
     if(isset($_POST["submit"])){
         if(!empty($_POST["titel"])){
         $titel = post(titel);
