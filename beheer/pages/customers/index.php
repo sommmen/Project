622a0d5b9a4be91ddb06040f7fe1d3269e2bdf83
kevin889 <kevin@pijning.nl>
2014-11-12 13:19:20 +0100

    bug fixes

--- beheer/pages/customers/index.php
+++ beheer/pages/customers/index.php
@@ -1,11 +1,14 @@
 
 
-<table><th>Voornaam</th>
-        <th>Achternaam</th>
+<table>
+    <tr>
+        <th>Naam</th>
         <th>Adres</th>
         <th>Postcode</th>
         <th>Woonplaats</th>
         <th>Email</th>
+        <th>Acties</th>
+    </tr>
 <?php
 
 /**
@@ -16,14 +19,13 @@ $query = "SELECT * FROM user";
 $result = $mysqli->query($query);
     while ($row = $result->fetch_object()) {
          echo '<tr>';
-         echo '<td>'. $row->name.' </td>'; 
-         echo '<td>'. $row->surname.' </td>';
+         echo '<td>'. $row->name.' '. $row->surname.'</td>';
          echo '<td>'. $row->address.' </td>';
          echo '<td>'. $row->zipcode.' </td>';
          echo '<td>'. $row->city.' </td>';
          echo '<td>'. $row->email.' </td>';
-         echo '<td> <a href="kbs.klanten.kevin889.nl/beheer/customers/edit/'. $row->id .'">Edit</a></td>';
-         echo '<td> <a href="kbs.klanten.kevin889.nl/beheer/customers/delete/'. $row->id .'">Delete</a></td>';
+         echo '<td> <a href="kbs.klanten.kevin889.nl/beheer/customers/edit/'. $row->id .'"><img alt="edit" src="/beheer/res/img/pencil90.png"></a> | ';
+         echo '<a href="kbs.klanten.kevin889.nl/beheer/customers/delete/'. $row->id .'"><img alt="edit" src="/beheer/res/img/black393.png"></a></td>';
          echo '</tr>';
     }
 ?>
