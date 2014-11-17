ed9a4fd730678e2fd2ecec9d51fef22f4d2e574a
sommmen <Dion_nl@yahoo.co.uk>
2014-11-17 16:45:37 +0100

    add.php checkboxje en in_nav en shit

--- beheer/pages/page/add.php
+++ beheer/pages/page/add.php
@@ -1,14 +1,17 @@
 <?php
+
 minRole(3);
 ?>
 <script type="text/javascript" src="javascript/slug.js"></script>
 <script type="text/javascript">
-function setSpanZero(){
-    if(document.getElementById('in_nav').value === 0){
-    document.getElementById('onZero').textContent = "niet tonen.";
+    function showIn_nav() {
+        if (document.getElementById('in_nav-checkbox').checked === true) {
+            document.getElementById('in_nav-number').style.display = "initial";
+            document.getElemntById('in_nav-text').innerHTML = "navigatievolgorde";
+        } else {
+            document.getElemntById('in_nav-text').innerHTML = "in navigatie?";
+        }
     }
-    setSpanZero();
-}
 </script>
 
 <a href="/beheer/page" class="button">Terug naar overzicht</a>
@@ -24,15 +27,16 @@ function setSpanZero(){
     <textarea rows="4" cols="50" name="body"></textarea>
     <label>publiceren</label>
     <input type="checkbox" name="publish" value="1">
-    <label>navigatievolgorde</label>
-    <input id="in_nav" type="number" name="in_nav" value="0"><span id="onZero"></span>
+    <label id="in_nav-text">in navigatie?</label>
+    <input id="in_nav-checkbox" type="checkbox" name="in_nav-checkbox" value="1" onchange="showIn_nav()">
+    <input id="in_nav-number" type="number" name="in_nav-number" value="0" style="display: none;">
+    <br>
     <input type="submit" name="submit" value="versturen">
 </form>
 
 <?php
-//TODO als je nu F5 drukt nadat je een pagina hebt toegevoegd, voegt hij de zelfde pagina weer toe.
+
 //TODO checken of de page slug al bestaat of niet. moet met ajax???
-//TODO liefst gwn een normale pagina toevoegen pagina, zodat je alles in 1 keer kan invullen. HEURISTIEKEN VAN NIELSONNAZI?
 
 if (isset($_POST["submit"])) {
     if (!empty($_POST["titel"])) {
@@ -40,27 +44,31 @@ if (isset($_POST["submit"])) {
         $description = post('description');
         $body = post('body');
         $slug = urlencode(strtolower(post('slug')));
-        if(post('publish') == 1){
+        if (post('publish') == 1) {
             $published = 1;
-        } else{
+        } else {
             $published = 0;
         }
-        $in_nav = post('in_nav');
+        if(post('in_nav-checkbox') == 1){
+            $in_nav = post('in_nav-number');
+        } else {
+            $in_nav = 0;
+        }
 
         $query = "SELECT * FROM page WHERE slug = \"$slug\"";
         $result = $mysqli->query($query);
         if ($mysqli->query($query)->num_rows == 0) {
-            if($mysqli->query("SELECT COUNT(*) FROM page WHERE title = '$titel' AND description = '$description' AND body = '$body' AND slug = '$slug' ") > 0){
+            if ($mysqli->query("SELECT COUNT(*) FROM page WHERE title = '$titel' AND description = '$description' AND body = '$body' AND slug = '$slug' ") > 0) {
                 redirect('/beheer/page');
             }
             $query = "INSERT INTO page (title, slug, published, in_nav, description, body) VALUES (\"$titel\",\"$slug\", \"$published\", \"$in_nav\", \"$description\", \"$body\")";
             if (!$mysqli->query($query)) {
                 echo $mysqli->error;
-            }else{
+            } else {
                 redirect('/beheer/page');
             }
-        } 
-    }else {
-            echo "vul een titel in.";
         }
+    } else {
+        echo "vul een titel in.";
+    }
 }
