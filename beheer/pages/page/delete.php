f9af282cb3bdc52c0b4762eb068886467f6f2f70
sommmen <Dion_nl@yahoo.co.uk>
2014-11-17 13:26:25 +0100

    delete.php werkt nu en is als het goed is waterdicht qua errors.

--- beheer/pages/page/delete.php
+++ beheer/pages/page/delete.php
@@ -3,21 +3,20 @@ minRole(3);
 
 $id = urlSegment(3);
 
-if(!is_int($id)){?>
+if(!is_int($id)){ ?>
 <meta http-equiv="refresh" content="5; url=/beheer/page" />
 <h1 style="color: red; font-size: 200%; text-align: center;">Error, er is iets misgegaan.</h1>
 <p style="text-align: center;"> u wordt teruggestuurt. </p>
-<?php}
+<?php }
 
 $check = $mysqli->query("SELECT * FROM page WHERE id = $id");
+$num_rows = $check->num_rows;
 
-if ($check->num_rows <= 0) {
-?>
+if ($num_rows <= 0) { ?>
 <meta http-equiv="refresh" content="5; url=/beheer/page" />
 <h1 style="color: red; font-size: 200%; text-align: center;">Error, deze pagina bestaat niet of is reeds verwijderd.</h1>
 <p style="text-align: center;"> u wordt teruggestuurt. </p>
-<?php
-} else {
+<?php } else {
     $mysqli->query("DELETE FROM page WHERE id = $id");
     redirect("page.php");
 }
