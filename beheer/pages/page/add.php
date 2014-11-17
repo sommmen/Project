5f7da8450a6126bbcc78dfe3d07d52f313f97b2d
sommmen <Dion_nl@yahoo.co.uk>
2014-11-17 13:35:35 +0100

    check of alle velden exact hetzelde in de db staan.

--- beheer/pages/page/add.php
+++ beheer/pages/page/add.php
@@ -20,7 +20,7 @@ minRole(3);
 
 <?php
 //TODO als je nu F5 drukt nadat je een pagina hebt toegevoegd, voegt hij de zelfde pagina weer toe.
-//TODO checken of de page slug al bestaat of niet.
+//TODO checken of de page slug al bestaat of niet. moet met ajax???
 //TODO liefst gwn een normale pagina toevoegen pagina, zodat je alles in 1 keer kan invullen. HEURISTIEKEN VAN NIELSONNAZI?
 
 if (isset($_POST["submit"])) {
@@ -33,6 +33,9 @@ if (isset($_POST["submit"])) {
         $query = "SELECT * FROM page WHERE slug = \"$slug\"";
         $result = $mysqli->query($query);
         if ($mysqli->query($query)->num_rows == 0) {
+            if($mysqli->query("SELECT COUNT(*) FROM page WHERE title = '$titel' AND description = '$description' AND body = '$body' AND slug = '$slug' ") > 0){
+                redirect('/beheer/page');
+            }
             $query = "INSERT INTO page (title, slug, published, in_nav, description, body) VALUES (\"$titel\",\"$slug\", 0, 0, \"$description\", \"$body\")";
             if (!$mysqli->query($query)) {
                 echo $mysqli->error;
