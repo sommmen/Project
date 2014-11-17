bd0faa1ae9bdac576745116566aa7a27af7239c7
sommmen <Dion_nl@yahoo.co.uk>
2014-11-17 18:46:44 +0100

    add.php met checks.

--- beheer/pages/page/add.php
+++ beheer/pages/page/add.php
@@ -10,6 +10,8 @@ minRole(3);
             document.getElemntById('in_nav-text').innerHTML = "navigatievolgorde";
         } else {
             document.getElemntById('in_nav-text').innerHTML = "in navigatie?";
+            document.getElementById('in_nav-number').style.display = "none";
+            document.getElementById('in_nav-number').value = "0";
         }
     }
 </script>
@@ -28,47 +30,52 @@ minRole(3);
     <label>publiceren</label>
     <input type="checkbox" name="publish" value="1">
     <label id="in_nav-text">in navigatie?</label>
-    <input id="in_nav-checkbox" type="checkbox" name="in_nav-checkbox" value="1" onchange="showIn_nav()">
+    <input id="in_nav-checkbox" type="checkbox" name="in_nav-checkbox" value="0" onchange="showIn_nav()">
     <input id="in_nav-number" type="number" name="in_nav-number" value="0" style="display: none;">
     <br>
     <input type="submit" name="submit" value="versturen">
 </form>
 
 <?php
-
-//TODO checken of de page slug al bestaat of niet. moet met ajax???
-
 if (isset($_POST["submit"])) {
     if (!empty($_POST["titel"])) {
-        $titel = post('titel');
-        $description = post('description');
-        $body = post('body');
-        $slug = urlencode(strtolower(post('slug')));
-        if (post('publish') == 1) {
-            $published = 1;
-        } else {
-            $published = 0;
-        }
-        if(post('in_nav-checkbox') == 1){
-            $in_nav = post('in_nav-number');
-        } else {
-            $in_nav = 0;
-        }
+        if (!empty($_POST["body"])) {
+            if (!(empty($_POST['slug']) || $mysqli->query("SELECT * FROM page WHERE slug = '$slug'")->num_rows > 0)) {
+                $titel = post('titel');
+                $description = post('description');
+                $body = post('body');
+                $slug = urlencode(strtolower(post('slug')));
+                if (post('publish') == 1) {
+                    $published = 1;
+                } else {
+                    $published = 0;
+                }
+                if (post('in_nav-checkbox') == 1) {
+                    $in_nav = post('in_nav-number');
+                } else {
+                    $in_nav = 0;
+                }
 
-        $query = "SELECT * FROM page WHERE slug = \"$slug\"";
-        $result = $mysqli->query($query);
-        if ($mysqli->query($query)->num_rows == 0) {
-            if ($mysqli->query("SELECT COUNT(*) FROM page WHERE title = '$titel' AND description = '$description' AND body = '$body' AND slug = '$slug' ") > 0) {
-                redirect('/beheer/page');
-            }
-            $query = "INSERT INTO page (title, slug, published, in_nav, description, body) VALUES (\"$titel\",\"$slug\", \"$published\", \"$in_nav\", \"$description\", \"$body\")";
-            if (!$mysqli->query($query)) {
-                echo $mysqli->error;
+                $query = "SELECT * FROM page WHERE slug = \"$slug\"";
+                $result = $mysqli->query($query);
+                if ($mysqli->query($query)->num_rows == 0) {
+                    if ($mysqli->query("SELECT COUNT(*) FROM page WHERE title = '$titel' AND description = '$description' AND body = '$body' AND slug = '$slug' ") > 0) {
+                        redirect('/beheer/page');
+                    }
+                    $query = "INSERT INTO page (title, slug, published, in_nav, description, body) VALUES (\"$titel\",\"$slug\", \"$published\", \"$in_nav\", \"$description\", \"$body\")";
+                    if (!$mysqli->query($query)) {
+                        echo $mysqli->error;
+                    } else {
+                        redirect('/beheer/page');
+                    }
+                }
             } else {
-                redirect('/beheer/page');
+                echo "Deze doelmap bestaat al.";
             }
+        } else {
+            echo "Vul text in.";
         }
     } else {
-        echo "vul een titel in.";
+        echo "Vul een titel in.";
     }
 }
