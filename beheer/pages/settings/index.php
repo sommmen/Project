c0b7e6836567705431b228f7600edf9e76092924
wfikkert <wfikkert@gmail.com>
2014-11-18 14:52:50 +0100

    settings

--- beheer/pages/settings/index.php
+++ beheer/pages/settings/index.php
@@ -1,4 +1,20 @@
 <?php
 minRole(3);
 
-echo "instellingen";
+?>
+<h1>Instellingen</h1>
+<table>
+    <th>key</th>
+    <th>value</th>
+<?php
+    $result = $mysqli->query("SELECT * FROM setting");
+    while($setting = $result->fetch_object()){
+ ?>   
+    <tr>
+        <td><?php echo $setting->key;?></td>
+        <td><?php echo $setting->value;?></a></td>
+    </tr> 
+    <?php
+    }
+    ?>
+</table>
