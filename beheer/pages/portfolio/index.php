481a577ab01d9e5322f8feb0d97ca8749452bed0
kevin889 <kevin@pijning.nl>
2014-11-18 13:43:24 +0100

    portfolio view toegevoegd

--- beheer/pages/portfolio/index.php
+++ beheer/pages/portfolio/index.php
@@ -1,3 +1,28 @@
 <?php
 minRole(3);
 ?>
+<a href="/beheer/portfolio/add" class="button blue">Album toevoegen</a>
+<h1>Portfolio albums</h1>
+
+<table>
+    <tr>
+        <th>Portfolio album</th><th width="75">Acties</th>
+    </tr>
+
+    <?php
+    $result = $mysqli->query("SELECT * FROM portfolio");
+    while($portfolio = $result->fetch_object()){
+        ?>
+        <tr>
+            <td><a href="/beheer/portfolio/view/<?php echo $portfolio->id;?>"><?php echo $portfolio->name;?></a></td>
+            <td>
+                <a href="/beheer/portfolio/edit/<?php echo $portfolio->id; ?>"><img src="/beheer/res/img/pencil90.png" alt="edit"/></a> |
+                <a href="/beheer/portfolio/delete/<?php echo $portfolio->id; ?>"><img src="/beheer/res/img/black393.png" alt="edit"/></a> |
+                <a href="/beheer/portfolio/addPhotos/<?php echo $portfolio->id;?>"><img src="/beheer/res/img/plus24.png" alt="add"/></a>
+            </td>
+        </tr>
+    <?php
+    }
+    ?>
+
+</table>
