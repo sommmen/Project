8d62957e0a12febfff8283edad999ad123b0d673
Dubehh <eelco1997@gmail.com>
2014-11-25 13:08:00 +0100

    User project overview
    Yay

--- beheer/pages/dashboard/index.php
+++ beheer/pages/dashboard/index.php
@@ -1,1 +1,26 @@
 <h1>Welkom <?php echo user_data('name').' '.user_data('surname');?></h1>
+
+<?php
+$id = user_data('id');
+
+function showProjects($uid){
+    global $mysqli;
+    $query = $mysqli->query('SELECT * FROM project WHERE uid = '.$uid.' ORDER BY created');
+    if($mysqli->error) return 404;
+    if($query ->num_rows == 0){
+        echo "<div class='alert-error'> U bent op dit moment nog niet gekoppeld aan een project</div>";
+    }
+    echo '<tr><thead>Project Naam</thead><thead>Aanmaakdatum</thead><thead>Items</thead></tr>';
+    while($row = $query->fetch_array()){
+        echo '<tr>';
+        echo '<td>'.$row['title'].'</td>';
+        echo '<td>'.$row['created'].'</td>';
+        echo "<td> <a href='beheer/customers/projectsView/". $row['id']."'>Foto's</a></td>";
+        echo '</tr>';
+    }
+}
+?>
+<table>
+    <?php showProjects($id)?>
+</table>
+
