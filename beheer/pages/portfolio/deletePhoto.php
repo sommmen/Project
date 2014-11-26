1b89abfca50704e616a8d0140313b3c40d60043f
sommmen <Dion_nl@yahoo.co.uk>
2014-11-26 13:56:14 +0100

    kevin je stinkt

--- beheer/pages/portfolio/deletePhoto.php
+++ beheer/pages/portfolio/deletePhoto.php
@@ -1,19 +1,19 @@
 <?php
 minRole(3);
 
-$query = "SELECT *, ph.portfolio_album as photo_portfolio_album FROM photo ph JOIN portfolio pr ON ph.portfolio_album = pr.portfolio_album WHERE ph.portfolio_album = '".urlSegment(3)."' LIMIT 1";
+$query = "SELECT *, ph.id as photo_id FROM photo ph JOIN portfolio pr ON ph.pid = pr.id WHERE ph.id = '".urlSegment(3)."' LIMIT 1";
 $result = $mysqli->query($query);
 if($result->num_rows == 0){
-    redirect('/beheer/portfolio');
+    redirect('/beheer/projects');
 }
 $photo = $result->fetch_object();
 
-$targetImage = dirname(__FILE__).'/../../../../uploads/'.sha1($photo->portfolio_album.$photo->name).'/'.$photo->file_name;
+$targetImage = dirname(__FILE__).'/../../../../uploads/'.sha1($photo->id.$photo->title).'/'.$photo->file_name;
 
 if(file_exists($targetImage) && is_file($targetImage)){
     if(unlink($targetImage)){
-        $mysqli->query("DELETE FROM photo WHERE portfolio_album = '".$photo->photo_portfolio_album."'");
-        redirect('/beheer/portfolio/view/'.$photo->portfolio_album);
+        $mysqli->query("DELETE FROM photo WHERE id = '".$photo->photo_id."'");
+        redirect('/beheer/portfolio/view/'.$photo->id);
     }else{
         redirect('/beheer/portfolio');
     }
