85e78465b81e120ddf2b568974dc845a4c4e21f2
sommmen <Dion_nl@yahoo.co.uk>
2014-11-26 11:02:53 +0100

    fix naar photo ipv photos

--- beheer/pages/portfolio/view.php
+++ beheer/pages/portfolio/view.php
@@ -1,20 +1,20 @@
 <?php
 minRole(3);
 
-$query = "SELECT * FROM project WHERE id = '".urlSegment(3)."'";
+$query = "SELECT * FROM portfolio WHERE id = '".urlSegment(3)."'";
 $result = $mysqli->query($query);
 if($result->num_rows == 0){
-    redirect('/beheer/projects');
+    redirect('/beheer/portfolio');
 }
-$project = $result->fetch_object();
+$portfolio = $result->fetch_object();
 
-$query = "SELECT * FROM photo WHERE pid = '".$project->id."'";
+$query = "SELECT * FROM photo WHERE portfolio_album = '".$portfolio->id."'";
 $result = $mysqli->query($query);
 ?>
-<a href="/beheer/projects" class="button">Terug naar overzicht</a>
-<a href="/beheer/projects/addPhotos/<?php echo $project->id;?>" class="button blue">Foto's toevoegen</a>
+<a href="/beheer/portfolio" class="button">Terug naar overzicht</a>
+<a href="/beheer/portfolio/addPhoto/<?php echo $portfolio->id;?>" class="button blue">Foto's toevoegen</a>
 
-<h1><?php echo $project->title;?></h1>
+<h1><?php echo $portfolio->name;?></h1>
 
 <section class="row">
 <?php
@@ -22,18 +22,18 @@ if($result->num_rows > 0) {
     while($photo = $result->fetch_object()) {
         ?>
         <figure <?php if($photo->selected == true){ echo 'class="selected"'; } ?>>
-            <a href="/thumb.php?photo=<?php echo $photo->id;?>&type=project" data-lightbox="image-1" data-title="<?php echo $photo->name;?>">
-                <img src="/thumb.php?photo=<?php echo $photo->id;?>&type=project" alt=""/>
+            <a href="/thumb.php?photo=<?php echo $photo->id;?>&type=portfolio" data-lightbox="image-1" data-title="<?php echo $photo->name;?>">
+                <img src="/thumb.php?photo=<?php echo $photo->id;?>&type=portfolio" alt=""/>
             </a>
             <figcaption>
                 <?php echo $photo->name; ?>
-                <a href="/beheer/projects/deletePhoto/<?php echo $photo->id;?>" onClick="return confirm('Weet je zeker dat je deze afbeelding wilt verwijderen?')"><img src="/beheer/res/img/black393.png" alt="delete"/></a>
+                <a href="/beheer/portfolios/deletePhoto/<?php echo $photo->id;?>" onClick="return confirm('Weet je zeker dat je deze afbeelding wilt verwijderen?')"><img src="/beheer/res/img/black393.png" alt="delete"/></a>
             </figcaption>
         </figure>
     <?php
     }
 } else {
-    echo 'Dit project heeft momenteel geen foto\'s. <a href="/beheer/projects/addPhotos/'.$project->id.'">Klik hier</a> om foto\'s toe te voegen.';
+    echo 'Dit portfolio heeft momenteel geen foto\'s. <a href="/beheer/portfolio/addPhoto/'.$portfolio->id.'">Klik hier</a> om foto\'s toe te voegen.';
 }
 ?>
 </section>
