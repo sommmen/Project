3848001a26f3b2a78e2bae77261fa9ffecce7b96
kevin889 <kevin@pijning.nl>
2014-11-26 14:06:33 +0100

    fixes

--- beheer/pages/portfolio/deletePhoto.php
+++ beheer/pages/portfolio/deletePhoto.php
@@ -3,15 +3,16 @@ minRole(3);
 
 //$query = "SELECT *, ph.id as photo_id FROM photo ph JOIN portfolio pr ON ph.pid = pr.id WHERE ph.id = '".urlSegment(3)."' LIMIT 1";
 
-$query = "SELECT *, ph.id as photo_id FROM photo ph JOIN portfolio po ON ph.portfolio_album = po.id WHERE ph.id = '".urlSegment(3)."' LIMIT1";
+$query = "SELECT *, ph.id as photo_id FROM photo ph LEFT JOIN portfolio po ON ph.portfolio_album = po.id WHERE ph.id = '".urlSegment(3)."' LIMIT 1";
 
 $result = $mysqli->query($query);
+echo $mysqli->error;
 if($result->num_rows == 0){
-    redirect('/beheer/projects');
+    redirect('/beheer/portfolio');
 }
 $photo = $result->fetch_object();
 
-$targetImage = dirname(__FILE__).'/../../../../uploads/'.sha1($photo->id.$photo->title).'/'.$photo->file_name;
+$targetImage = dirname(__FILE__).'/../../../../uploads/'.sha1($photo->id.$photo->name).'/'.$photo->file_name;
 
 if(file_exists($targetImage) && is_file($targetImage)){
     if(unlink($targetImage)){
