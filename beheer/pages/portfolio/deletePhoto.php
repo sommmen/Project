664532c28eeb41b9ee2b5f07645070e3931ce5f9
sommmen <Dion_nl@yahoo.co.uk>
2014-11-26 11:38:38 +0100

    adsadsadsad

--- beheer/pages/portfolio/deletePhoto.php
+++ beheer/pages/portfolio/deletePhoto.php
@@ -1,22 +1,22 @@
 <?php
 minRole(3);
 
-$query = "SELECT *, ph.id as photo_id FROM photo ph JOIN project pr ON ph.pid = pr.id WHERE ph.id = '".urlSegment(3)."' LIMIT 1";
+$query = "SELECT *, ph.id as photo_portfolio_album FROM photo ph JOIN portfolio pr ON ph.pportfolio_album = pr.portfolio_album WHERE ph.portfolio_album = '".urlSegment(3)."' LIMIT 1";
 $result = $mysqli->query($query);
 if($result->num_rows == 0){
-    redirect('/beheer/projects');
+    redirect('/beheer/portfolio');
 }
 $photo = $result->fetch_object();
 
-$targetImage = dirname(__FILE__).'/../../../../uploads/'.sha1($photo->id.$photo->title).'/'.$photo->file_name;
+$targetImage = dirname(__FILE__).'/../../../../uploads/'.sha1($photo->portfolio_album.$photo->name).'/'.$photo->file_name;
 
 if(file_exists($targetImage) && is_file($targetImage)){
     if(unlink($targetImage)){
-        $mysqli->query("DELETE FROM photo WHERE id = '".$photo->photo_id."'");
-        redirect('/beheer/projects/view/'.$photo->id);
+        $mysqli->query("DELETE FROM photo WHERE portfolio_album = '".$photo->photo_portfolio_album."'");
+        redirect('/beheer/portfolio/view/'.$photo->portfolio_album);
     }else{
-        redirect('/beheer/projects');
+        redirect('/beheer/portfolio');
     }
 }else{
-     redirect('/beheer/projects');
+     redirect('/beheer/portfolio');
 }
