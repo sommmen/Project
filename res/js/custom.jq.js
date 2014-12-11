fda7a63528c5aa7277698cc193572e701b2a674f
sommmen <Dion_nl@yahoo.co.uk>
2014-12-11 14:45:26 +0100

    jquery shizzle

--- res/js/custom.jq.js
+++ res/js/custom.jq.js
@@ -1,20 +1,36 @@
-$(document).ready(function(){
-	
-	$("#mobileMenu").click(function(){
-		$("nav").slideToggle();
-	});
+$(document).ready(function () {
+
+    $("#mobileMenu").click(function () {
+        $("nav").slideToggle();
+    });
 
 
-    if($(window).width < 749) {
+    if ($(window).width < 749) {
         $(".main").click(function () {
             $("nav").slideUp();
         });
     }
-	
-	$(window).resize(function(){
-		if($(this).width() > 749){
-			$("nav").show();
-		}
-	});
+
+    $(window).resize(function () {
+        if ($(this).width() > 749) {
+            $("nav").show();
+        }
+    });
+
+    $("#toggleDropDown").click(function () {
+        $("#dropdown").slideToggle("slow");
+        $("#loginform").slideToffle("slow");
+
+        var test = $("#toggleDropDown").text();
+        
+        if (test === "Wachtwoord vergeten") {
+          $("#toggleDropDown").text("Terug");
+          $("section > header").text("Nieuw Wachtwoord");
+        } else {
+            $("#toggleDropDown").text("Wachtwoord vergeten");
+            $("section > header").text("Login");
+        }
+        
+    });
 
 });
