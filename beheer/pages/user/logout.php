c798e8970a35f8a080320c29a9114033da4ec872
sommmen <Dion_nl@yahoo.co.uk>
2015-01-07 12:03:16 +0100

    comments/fixjes

--- beheer/pages/user/logout.php
+++ beheer/pages/user/logout.php
@@ -1,6 +1,13 @@
 <?php
+/*
+ *              in al haar professionaliteit gemaakt door:
+ *                          Jurrian Fisher
+ */
+
 $token = $_SESSION['user']['token'];
 $username = $_SESSION['user']['name'];
+
+//verwijder het token (hierdoor zal op elke pagina de minrole functie foutief zijn en de uitgelogde gebruiker terugsturen.)
 $mysqli->query("UPDATE user SET token = '' WHERE username = '" . $username . "' AND token = '" . $token . "'");
 
 session_destroy();
