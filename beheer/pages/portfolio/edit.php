0a479a5a07a8a0e84db50c2e85c63086f1af408f
wfikkert <wfikkert@gmail.com>
2015-01-07 12:33:31 +0100

    portfolio comments

--- beheer/pages/portfolio/edit.php
+++ beheer/pages/portfolio/edit.php
@@ -6,19 +6,19 @@ if($result->num_rows==1){
     $portfolio=$result->fetch_object();
     if(isset($_POST['submit'])) {
         
-        $targerPath = dirname(__FILE__) . '/../../../../uploads/' . sha1($portfolio->id . $portfolio->name) . '/';
+        $targerPath = dirname(__FILE__) . '/../../../../uploads/' . sha1($portfolio->id . $portfolio->name) . '/'; //kijkt welke foto's in het album zit.
         $name= post('naam');
-        $result = $mysqli->query("UPDATE portfolio SET name = '".$name."' WHERE id = '".$portfolio->id."'");
+        $result = $mysqli->query("UPDATE portfolio SET name = '".$name."' WHERE id = '".$portfolio->id."'"); //de naam van het album wordt geupdate
         
         $result = $mysqli->query("SELECT * FROM portfolio WHERE id = '" . $portfolio->id . "'");
             if ($result->num_rows > 0) {
                 $portfolio = $result->fetch_object();
 
-                $newPath = dirname(__FILE__) . '/../../../../uploads/' . sha1($portfolio->id . $portfolio->name) . '/';
-                rename($targerPath, $newPath);
+                $newPath = dirname(__FILE__) . '/../../../../uploads/' . sha1($portfolio->id . $portfolio->name) . '/'; //kijkt wat de directory van het portfolio is.
+                rename($targerPath, $newPath); //de targerPath wordt newPath zodat alle foto's naar de nieuwe map gestuurd worden.
             }
         if($result){
-            setMessage("Album succesvol bijgewerkt.");
+            setMessage("Album succesvol bijgewerkt."); //als het succesvol geupdate is wordt dit bericht op de pagina getoon en wordt je teruggestuurd naar de index file van portfolio.
             redirect('/beheer/portfolio');   
         }else{
             echo $mysqli->error;
